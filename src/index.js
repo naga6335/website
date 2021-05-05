e2e6c0503898d2151c9a0d9b046fac38d582bb44
naga6335 <y.nagami9970@gmail.com>
2021-05-05 15:09:36 +0900

    ReactNote

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return null;
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
+/* <App />をrenderし、index.htmlのルートid要素を取得している */
