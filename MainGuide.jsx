124c8a5d7f84e2dedc98591796c548333fbcfa04
naga6335 <y.nagami9970@gmail.com>
2021-05-10 23:46:38 +0900

    ReactNotes

--- MainGuide.jsx
+++ MainGuide.jsx
@@ -4,30 +4,49 @@ import App from "./App"
 
 ReactDOM.render(<App />, document.getElementById("root"));
 /*
+
+
 <App />をrenderし、index.htmlのルートid要素を取得している。
 ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+
+
 component
 => 画面要素の1つの単位（1画面から、1テキストボックスまで様々）
 
+
 ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+
+
 Props
 => コンポーネントに渡される引数的なもの。
 
+
 ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+
+
 useState (const [ 自由な値, set自由な値 ] = useState(初期値);)
 => 各コンポーネートが持つ状態(可変)。動的に条件に応じ様々な情報を表示できる。
 
+
 ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+
+
 useEffect (() => {}, []);
 => 関心の分離をしたいときに使う　第2引数に配列を置き、空、変数、複数の変数を持つことができる
   そのコンポーネントで表示したい1回のみ走らせたい処理を書くことができる。
   変数が変化した時のみ、中の処理が走る。
 
+
 ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+
+
 memo
 => import { memo } from "react"; 関数処理の前にmemo()記載でレンダリングの最適化ができる。
 
+
 ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+
+
 useCallback
 => import必要。useEffectと似ている。useCallback(()=>{関数}, [依存配列])
    アロー関数で書いた関数はpropsとしては毎回新しい関数を生成しているという判断になり、レンダリングされてしまう。
@@ -36,6 +55,8 @@ useCallback
 
 
 ーーーーーーーーーーーーーーーーーーーーー　styleに関して　ーーーーーーーーーーーーーーーーーーーーーーーーー
+
+
 styled-components
 => import styled from 'styled-components';で
    const Container = styled.div`
@@ -49,6 +70,8 @@ styled-components
 
 
 ーーーーーーーーーーーーーーーーーーーーー　画面遷移に関して　ーーーーーーーーーーーーーーーーーーーーーーーーーー
+
+
 React Router
 => import { BrowserRouter, Link, Switch, Route } from "react-router-dom";をimportし
   リンクさせたいコンポーネントに<BrowserRouter>　→　<Link to = "">　→　<Switch>　→　<Route path = "">の順で囲む。
@@ -77,6 +100,7 @@ useHistory Linkを使わないページ遷移
 
 ーーーーーーーーーーーーーーーーーーー　コンポーネントの分割に関して　ーーーーーーーーーーーーーーーーーーーーーーーー
 
+
 Atomic Design
 画面要素を ATOMS, MOLECULES, ORGANISMS, TEMPLATES, PAGESの5段階に分け組み合わせることでUIを実現
 コンポーネント化された要素が画面を構成しているという考え。React,Vue用というわけではない
@@ -93,6 +117,7 @@ ex)atoms
     return ( <SButton>{ children }</SButton>)
   };　のように分割代入することで使い回しができる。
   styleベタがき、切り出してstyle.button``,(BaseButton)``でスタイリングもできる。
+
   components/src/atoms
   components/src/molecules
   components/src/organism
@@ -101,6 +126,7 @@ ex)atoms
 
 
 ーーーーーーーーーーーーーーーーーーー　グローバルなstate管理について　ーーーーーーーーーーーーーーーーーーーーーーーー
+
 
 Contextでのstate管理
   providersフォルダを作成 => import { createContext } from "react";
@@ -114,4 +140,16 @@ Contextでのstate管理
 
   Recoilライブラリでグローバルなstate管理も簡単に可能。
   少ない記述、useSetRecoilStateで更新関数のみ受け取ることで、勝手に不要な再レンダリングを制御してくれる。
+
+
+ーーーーーーーーーーーーーーーーーーー　TypeScript　ーーーーーーーーーーーーーーーーーーーーーーーー
+
+
+typeList.tsxを参照。型定義をすることでエラー検知、補完機能がつくためバグを予防できる。
+  props: Pick<TodoType, "~~~" | "~~~" >
+  props: Omit<TodoType, "~~~" >
+  で他フォルダから参照し型指定ができる。オブジェクトが少ない場合はOmitの方がスッキリ書ける。
+
+
+
 */
--- typeList.tsx
+++ typeList.tsx
@@ -9,8 +9,9 @@ let num: number = 0;
 // string
 let str: string = "A";
 
-// Array 配列
+// Array 配列　map関数と合わせてよく使う。
 let arr1: Array<number> = [0, 1, 2];
+
 
 // tuple
 let tuple: [number, string] = [0, "A"];
@@ -34,3 +35,12 @@ let obj1: object = {};
 let obj2: { id: number, name: string } = { id: 0, name: "AAA" };
 
 /* tsconfigで設定可能 strictモードの解除など */
+
+
+/* 別ファイルに切り出してインポートしながら使う。　ex) /types/todo.tsx*/
+export type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
